45b428e51188ea211bea7053fa41fd131535a325
AlsAbrego <39961760+AlsAbrego@users.noreply.github.com>
2021-08-12 21:56:17 -0300

    Adicionando a anotação de Repositório
    Adicionando a anotação de Repositório @Repository

--- projeto1/src/main/java/com/springBoot_projeto1/projeto1/repository/JornadaRepository.java
+++ projeto1/src/main/java/com/springBoot_projeto1/projeto1/repository/JornadaRepository.java
@@ -3,5 +3,6 @@ package com.springBoot_projeto1.projeto1.repository;
 import com.springBoot_projeto1.projeto1.model.JornadaTrabalho;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface JornadaRepository  extends JpaRepository<JornadaTrabalho, Long> {
 }
